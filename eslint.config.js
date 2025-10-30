e957a53dbce1ab1b04960b7a77b7fafe90e39dbc
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-10-30 22:19:20 +0700

    change sort import plugin from eslint to prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,6 @@
 import js from '@eslint/js'
-import prettier from 'eslint-config-prettier'
-import importPlugin from 'eslint-plugin-import'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
@@ -17,12 +14,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
-      prettier,
     ],
-    plugins: {
-      import: importPlugin,
-      'simple-import-sort': simpleImportSort,
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -30,25 +22,6 @@ export default defineConfig([
     rules: {
       semi: ['error', 'never'],
       quotes: ['error', 'single'],
-      'simple-import-sort/imports': [
-        'error',
-        {
-          groups: [
-            [
-              '^react',
-              '^@?\\w',
-              '^/',
-              '^(@|src)(/.*|$)',
-              '^\\.',
-              '^.+\\.(css|scss|sass|less)$',
-            ],
-          ],
-        },
-      ],
-      'simple-import-sort/exports': 'error',
-      'import/first': 'error',
-      'import/newline-after-import': 'off',
-      'import/no-duplicates': 'error',
     },
   },
 ])

801e467bf47da17ddb4de8fe37be5d3748255372
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2025-11-08 05:33:28 +0700

    add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { Navbar } from './components'
+import { Hero, Navbar } from './components'
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <main className="relative min-h-screen overflow-x-hidden">
+      <div
+        className="absolute -top-28 -left-28 w-96 h-96 bg-linear-to-tr from-indigo-500/20 to-pink-500/20 rounded-full
+          blur-3xl -z-10"
+      ></div>
+      <div className="overflow-hidden">
+        <Navbar />
+        <Hero />
+      </div>
+    </main>
   )
 }
 
